2eb58de02458473ac7e1f56c4d30f362f01d206a
knut <knut@kubernetes.science-computing.de>
2017-11-21 15:06:58 +0100

    Initial version

---
+++ src/linuxlunch.cr
@@ -0,0 +1,24 @@
+require "./linuxlunch/*"
+
+require "kemal"
+
+before_all do |env|
+  env.response.content_type = "application/json"
+end
+
+get "/api/v1/message" do |env|
+  { "hello" => "world" }.to_json
+end
+
+post "/api/v1/update" do |env|
+begin
+  name = env.params.json["name"]
+  message = env.params.json["message"]
+  { "response" => "#{name} says: #{message}" }.to_json
+rescue e: KeyError
+  { "response" => "missing parameter: #{e}" }.to_json
+end
+end
+
+Kemal.run
+
